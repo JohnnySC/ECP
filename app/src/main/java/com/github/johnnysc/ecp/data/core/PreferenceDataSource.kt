15331501b4c95a9b118080d81c368fa559c9731e
Oleksandr Putsenko <alexputsenko@ukr.net>
2021-12-06 21:10:32 +0200

    ECP-0040 interface and base class added

---
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/PreferenceDataSource.kt
@@ -0,0 +1,25 @@
+package com.github.johnnysc.ecp.data.core
+
+import android.content.SharedPreferences
+
+interface PreferenceDataSource : IntStorage, StringStorage {
+    class Base (private val sharedPreferences: SharedPreferences) : PreferenceDataSource {
+
+        override fun save(key: String, value: Int) {
+            sharedPreferences.edit().putInt(key, value).apply()
+        }
+
+        override fun save(key: String, value: String) {
+            sharedPreferences.edit().putString(key, value).apply()
+        }
+
+        override fun read(key: String, defaultValue: Int): Int {
+            return sharedPreferences.getInt(key, defaultValue)
+        }
+
+        override fun read(key: String, defaultValue: String): String {
+            return sharedPreferences.getString(key, defaultValue) ?: defaultValue
+        }
+    }
+
+}
