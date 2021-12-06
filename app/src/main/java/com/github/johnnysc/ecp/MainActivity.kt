94d4e9b5e5a5357bdf0f945d41d058ab1aab2db8
user <jjonik957@gmail.com>
2021-12-06 15:28:04 +0500

    ECP-0005 Поставил в манифестео и в activity риентацию портретную

--- app/src/main/java/com/github/johnnysc/ecp/MainActivity.kt
+++ app/src/main/java/com/github/johnnysc/ecp/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.github.johnnysc.ecp
 
+import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
 }
