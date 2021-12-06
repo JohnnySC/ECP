02ed3752ab2e4aa00aa0832de8a60d81e2a1fcb3
Suzanna <zeynalovasa@mail.ru>
2021-12-06 22:49:35 +0400

    ECP-0020: Dispatchers.Base

--- app/src/main/java/com/github/johnnysc/ecp/ui/core/Dispatchers.kt
+++ app/src/main/java/com/github/johnnysc/ecp/ui/core/Dispatchers.kt
@@ -1,10 +1,34 @@
 package com.github.johnnysc.ecp.ui.core
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 interface Dispatchers {
 
     fun launchUI(scope: CoroutineScope, block: suspend () -> Unit): Job
     fun launchBackground(scope: CoroutineScope, block: suspend () -> Unit): Job
     suspend fun changeToUI(block: suspend () -> Unit)
+
+    class Base: Dispatchers {
+
+        private val main = kotlinx.coroutines.Dispatchers.Main
+        private val io = kotlinx.coroutines.Dispatchers.IO
+
+        override fun launchUI(scope: CoroutineScope, block: suspend () -> Unit) =
+            scope.launch(main) {
+                block.invoke()
+            }
+
+        override fun launchBackground(scope: CoroutineScope, block: suspend () -> Unit) =
+            scope.launch(io) {
+                block.invoke()
+            }
+
+        override suspend fun changeToUI(block: suspend () -> Unit) =
+            withContext(main) {
+                block.invoke()
+            }
+    }
 }
