57db407a36168e2b5de66d02cd53405a9c4cdf19
Metamorfag <47786878+Metamorfag@users.noreply.github.com>
2021-12-06 14:26:17 +0200

    ECP-0054-serialization - added ListSerialization implementation

--- app/src/main/java/com/github/johnnysc/ecp/data/core/ListSerialization.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/ListSerialization.kt
@@ -4,4 +4,10 @@ interface ListSerialization {
     fun map(list: List<String>): String
 
     fun map(data: String): List<String>
+
+    class Base : ListSerialization {
+        override fun map(list: List<String>) = list.joinToString(", ")
+
+        override fun map(data: String) = data.split(", ")
+    }
 }
