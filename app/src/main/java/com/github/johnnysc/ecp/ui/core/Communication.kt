e484f48fb0fad0063f740f1ca4f15ca3e89ce2d4
Oleksandr Putsenko <alexputsenko@ukr.net>
2021-12-07 21:36:06 +0200

    ECP-0029 base class added

--- app/src/main/java/com/github/johnnysc/ecp/ui/core/Communication.kt
+++ app/src/main/java/com/github/johnnysc/ecp/ui/core/Communication.kt
@@ -1,6 +1,20 @@
 package com.github.johnnysc.ecp.ui.core
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.github.johnnysc.ecp.core.Mapper.UnitMapper
 
 interface Communication<T> : Observe<T>, UnitMapper<T> {
+    class Base<T> : Communication<T> {
+        private val mutableLiveData: MutableLiveData<T> = MutableLiveData()
+
+        override fun map(data: T) {
+            mutableLiveData.value = data
+        }
+
+        override fun observe(owner: LifecycleOwner, observer: Observer<T>) {
+            mutableLiveData.observe(owner, observer)
+        }
+    }
 }
