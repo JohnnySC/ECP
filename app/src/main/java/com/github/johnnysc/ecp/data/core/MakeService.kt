4e18dba2ac178ff5c0befc1f9a8ac95a7b18d366
Елизавета Белова <EPBelova.SBT@sberbank.ru>
2021-12-07 21:43:48 +0300

    ECP-0050 fix due to comments

--- app/src/main/java/com/github/johnnysc/ecp/data/core/MakeService.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/MakeService.kt
@@ -4,11 +4,11 @@ import retrofit2.Retrofit
 
 interface MakeService {
     fun <T> service(clazz: Class<T>): T
-}
 
-class Base(provideRetrofit: ProvideRetrofit) : MakeService {
+    class Base(provideRetrofit: ProvideRetrofit) : MakeService {
 
-    private val retrofit = provideRetrofit.retrofit()
+        private val retrofit = provideRetrofit.retrofit()
 
-    override fun <T> service(clazz: Class<T>): T = retrofit.create(clazz)
+        override fun <T> service(clazz: Class<T>): T = retrofit.create(clazz)
+    }
 }
