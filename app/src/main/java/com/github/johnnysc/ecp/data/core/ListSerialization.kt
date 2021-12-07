ba799ac0fc5d8e38c236d72c86540e7ecec0bc34
Metamorfag <47786878+Metamorfag@users.noreply.github.com>
2021-12-07 22:03:07 +0200

    ECP-0055-listSerialization.Base - added ListSerialization implementation

--- app/src/main/java/com/github/johnnysc/ecp/data/core/ListSerialization.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/ListSerialization.kt
@@ -6,13 +6,14 @@ interface ListSerialization {
     fun map(data: String): List<String>
 
     class Base : ListSerialization {
+
         override fun map(list: List<String>): String {
-            var convertedString = ""
+            val convertedString = StringBuilder()
             when (list.isNotEmpty()) {
-                list.size > 1 -> list.forEach { convertedString += it + DELIMITER }
-                list.size == 1 -> list.forEach { convertedString = it }
+                list.size > 1 -> list.forEach { convertedString.append(it + DELIMITER) }
+                list.size == 1 -> list.forEach { convertedString.append(it) }
             }
-            return convertedString.trimEnd(',')
+            return convertedString.trimEnd(',').toString()
         }
 
         override fun map(data: String) = data.split(DELIMITER)
