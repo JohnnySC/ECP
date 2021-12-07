9dfbf261e8a4ebfa7b5a7285065aeb294d0f4a5f
Oleksandr Putsenko <alexputsenko@ukr.net>
2021-12-07 23:06:34 +0200

    ECP-0045 base class added

--- app/src/main/java/com/github/johnnysc/ecp/data/core/ProvideInterceptor.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/ProvideInterceptor.kt
@@ -1,8 +1,18 @@
 package com.github.johnnysc.ecp.data.core
 
 import okhttp3.Interceptor
+import okhttp3.logging.HttpLoggingInterceptor
 
 interface ProvideInterceptor {
 
     fun interceptor(): Interceptor
+    class Base(private val isDebug: Boolean) {
+        private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor()
+
+        fun interceptorLevel(): HttpLoggingInterceptor {
+            interceptor.level =
+                if (isDebug) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+            return interceptor
+        }
+    }
 }
