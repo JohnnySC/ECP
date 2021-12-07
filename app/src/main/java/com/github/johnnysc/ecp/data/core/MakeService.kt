500a670630fe4ef361096644a08d228e5af89add
Елизавета Белова <EPBelova.SBT@sberbank.ru>
2021-12-07 15:22:15 +0300

    ECP-0050 added MakeService implementation

--- app/src/main/java/com/github/johnnysc/ecp/data/core/MakeService.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/MakeService.kt
@@ -1,6 +1,19 @@
 package com.github.johnnysc.ecp.data.core
 
-interface MakeService {
+import retrofit2.Retrofit
 
+interface MakeService {
     fun <T> service(clazz: Class<T>): T
+}
+
+class Base(provideRetrofit: ProvideRetrofit) : MakeService {
+
+    private var retrofit : Retrofit
+
+    init {
+        retrofit = provideRetrofit.retrofit()
+    }
+
+    override fun <T> service(clazz: Class<T>): T = retrofit.create(clazz)
+
 }
