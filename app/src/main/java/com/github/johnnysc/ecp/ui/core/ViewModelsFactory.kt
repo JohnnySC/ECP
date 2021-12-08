ee739f46cb9198c86f6ea054738c85c0d982effd
Oleksandr Putsenko <alexputsenko@ukr.net>
2021-12-09 00:27:41 +0200

    ECP-0057 class added

---
+++ app/src/main/java/com/github/johnnysc/ecp/ui/core/ViewModelsFactory.kt
@@ -0,0 +1,11 @@
+package com.github.johnnysc.ecp.ui.core
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.github.johnnysc.ecp.data.core.DependencyContainer
+
+class ViewModelsFactory(private val dependencyContainer: DependencyContainer) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
+        dependencyContainer.module(modelClass).viewModel()
+}
