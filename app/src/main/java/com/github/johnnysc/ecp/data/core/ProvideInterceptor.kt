85fdbe86fbbdc0990e428ca98371b867936a48d8
Alexandr Putsenko <83665333+evilstan@users.noreply.github.com>
2021-12-08 14:00:49 +0200

    ECP-0044 base class added (#89)
    * ECP-0044 base class added
    
    * ECP-0044 interface implementation fixed
    
    * ECP-0048 interface implemented
    
    * ECP-0044 apply used
    
    Co-authored-by: evilstan <evilstan111@gmail.com>

--- app/src/main/java/com/github/johnnysc/ecp/data/core/ProvideInterceptor.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/ProvideInterceptor.kt
@@ -1,8 +1,20 @@
 package com.github.johnnysc.ecp.data.core
 
 import okhttp3.Interceptor
+import okhttp3.logging.HttpLoggingInterceptor
 
 interface ProvideInterceptor {
 
     fun interceptor(): Interceptor
+
+    class Base(private val isDebug: Boolean) : ProvideInterceptor {
+
+        private val interceptor = HttpLoggingInterceptor().apply {
+            level = if (isDebug) HttpLoggingInterceptor.Level.BODY
+            else HttpLoggingInterceptor.Level.NONE
+        }
+
+        override fun interceptor(): Interceptor = interceptor
+
+    }
 }
