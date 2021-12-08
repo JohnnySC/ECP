5ecc919acad0b23b398c368a880a7ebedaa652d8
HardSea <androidmen2000@gmail.com>
2021-12-08 14:02:46 +0200

    ECP-0046: Implement ProvideOkHttpClientBuilder (#87)
    * ECP-0046: Implement ProvideOkHttpClientBuilder
    
    * ECP-0046: Fix implementation
    
    * ECP-0046: Cleanup code
    
    * ECP-0046: Move constants to companion object

--- app/src/main/java/com/github/johnnysc/ecp/data/core/ProvideOkHttpClientBuilder.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/ProvideOkHttpClientBuilder.kt
@@ -1,7 +1,26 @@
 package com.github.johnnysc.ecp.data.core
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
 
 interface ProvideOkHttpClientBuilder {
     fun okHttpClientBuilder(): OkHttpClient.Builder
+
+    class Base(
+        private val interceptor: Interceptor,
+        private val readTimeout: Long = READ_TIMEOUT,
+        private val connectTimeout: Long = CONNECT_TIMEOUT
+    ) : ProvideOkHttpClientBuilder {
+
+        companion object {
+            private const val READ_TIMEOUT = 60L
+            private const val CONNECT_TIMEOUT = 60L
+        }
+
+        override fun okHttpClientBuilder() = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .readTimeout(readTimeout, TimeUnit.SECONDS)
+            .connectTimeout(connectTimeout, TimeUnit.SECONDS)
+    }
 }
