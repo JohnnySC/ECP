0f5892f23156ab86fd960b1be947fca9117544e9
Metamorfag <47786878+Metamorfag@users.noreply.github.com>
2021-12-09 18:10:58 +0200

    ECP-0062: NavigationCommunication - fix NavigationCommunication interface and implementation

--- app/src/main/java/com/github/johnnysc/ecp/ui/core/NavigationCommunication.kt
+++ app/src/main/java/com/github/johnnysc/ecp/ui/core/NavigationCommunication.kt
@@ -1,7 +1,5 @@
 package com.github.johnnysc.ecp.ui.core
 
-import com.github.johnnysc.ecp.ui.NavigationUi
-
 interface NavigationCommunication : Communication<NavigationUi> {
     class Base : Communication.Single<NavigationUi>(), NavigationCommunication
 }
