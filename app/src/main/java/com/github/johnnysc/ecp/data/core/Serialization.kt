e64d93304b77273d9d60e80b0fdac103d9596d0a
Елизавета Белова <belka0107@ya.ru>
2021-12-09 13:00:30 +0300

    ECP-0037 one more fix

--- app/src/main/java/com/github/johnnysc/ecp/data/core/Serialization.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/Serialization.kt
@@ -7,8 +7,7 @@ interface Serialization {
     fun convertToString(data: Serializable): String
     fun <T : Serializable> convertFromString(key: String, clazz: Class<T>): T
 
-    class Base : Serialization {
-        val gson = Gson()
+    class Base(private val gson : Gson) : Serialization {
 
         override fun convertToString(data: Serializable): String = gson.toJson(data)
 
