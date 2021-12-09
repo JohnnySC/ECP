ea81270429949858c1c94fce0f8bc3f21233e81c
belka0107 <belka0107@ya.ru>
2021-12-09 13:04:33 +0300

    ECP-0037 added base serialization (#99)
    * ECP-0037 added base serialization

--- app/src/main/java/com/github/johnnysc/ecp/data/core/Serialization.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/Serialization.kt
@@ -1,8 +1,16 @@
 package com.github.johnnysc.ecp.data.core
 
+import com.google.gson.Gson
 import java.io.Serializable
 
 interface Serialization {
     fun convertToString(data: Serializable): String
     fun <T : Serializable> convertFromString(key: String, clazz: Class<T>): T
+
+    class Base(private val gson : Gson) : Serialization {
+
+        override fun convertToString(data: Serializable): String = gson.toJson(data)
+
+        override fun <T : Serializable> convertFromString(key: String, clazz: Class<T>): T = gson.fromJson(key, clazz)
+    }
 }
