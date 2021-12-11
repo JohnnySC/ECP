efd4d5acff2e12de03d82bc0ecca3e6cb909080d
VnukovNS <95089447+VnukovNS@users.noreply.github.com>
2021-12-11 19:04:58 +0300

    feature/ECP-0071 AfterTextChanged создал абстракт класс с деф методами от TextWatcher

---
+++ app/src/main/java/com/github/johnnysc/ecp/ui/core/AfterTextChanged.kt
@@ -0,0 +1,19 @@
+package com.github.johnnysc.ecp.ui.core
+
+import android.text.Editable
+import android.text.TextWatcher
+
+abstract class AfterTextChanged : TextWatcher {
+    override fun beforeTextChanged(var1: CharSequence, var2: Int, var3: Int, var4: Int){
+
+    }
+
+    override fun onTextChanged(var1: CharSequence, var2: Int, var3: Int, var4: Int){
+
+    }
+
+    override fun afterTextChanged(var1: Editable){
+
+    }
+
+}
