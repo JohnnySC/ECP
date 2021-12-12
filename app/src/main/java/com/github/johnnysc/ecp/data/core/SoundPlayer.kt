4b5512a01655be7f17ee064d24f160966e1d7f36
Елизавета Белова <belka0107@ya.ru>
2021-12-12 22:16:18 +0300

    ECP-0075 added base soundplayer

--- app/src/main/java/com/github/johnnysc/ecp/data/core/SoundPlayer.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/SoundPlayer.kt
@@ -1,7 +1,29 @@
 package com.github.johnnysc.ecp.data.core
 
+import android.net.Uri
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+
 interface SoundPlayer {
     fun play(url:String)
 
     fun stop()
+
+    class BaseSoundPlayer(private val player : ExoPlayer) : SoundPlayer{
+        override fun play(url: String) {
+            val media = MediaItem.fromUri(Uri.parse(url))
+            player.run {
+                setMediaItem(media)
+                prepare()
+                play()
+            }
+        }
+
+        override fun stop() {
+            player.run {
+                stop()
+                release()
+            }
+        }
+    }
 }
