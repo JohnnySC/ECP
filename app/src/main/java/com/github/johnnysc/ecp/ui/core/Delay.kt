48d7db03b00fe647a2072c7206f42e7809685271
Dima Variukhin <47786878+Dima-Variukhin@users.noreply.github.com>
2021-12-13 15:22:48 +0200

    ECP-0069: Delay - added Delay interface and implementation (#124)
    Co-authored-by: Metamorfag <47786878+Metamorfag@users.noreply.github.com>

---
+++ app/src/main/java/com/github/johnnysc/ecp/ui/core/Delay.kt
@@ -0,0 +1,26 @@
+package com.github.johnnysc.ecp.ui.core
+
+import java.util.*
+
+interface Delay {
+    fun delay(text: String)
+
+    class Base(
+        private val time: Long = 300L,
+        private val block: (String) -> Unit,
+    ) : Delay {
+        private var timer = Timer()
+        override fun delay(text: String) {
+            timer.cancel()
+            timer = Timer()
+            timer.schedule(
+                object : TimerTask() {
+                    override fun run() {
+                        block.invoke(text)
+                    }
+                },
+                time
+            )
+        }
+    }
+}
