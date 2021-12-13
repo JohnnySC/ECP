34875d11cca5bc88bb74ca443f6e5c3dd47041c7
Елизавета Белова <belka0107@ya.ru>
2021-12-13 22:49:17 +0300

    DCT-0012 added PhoneticCache

---
+++ app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/PhoneticCache.kt
@@ -0,0 +1,8 @@
+package com.github.johnnysc.ecp.data.dictionary.cache
+
+import io.realm.RealmObject
+
+interface PhoneticCache {
+
+    open class Base(val text: String = "", var audio: String = "") : RealmObject()
+}
