c4961fcf2f339e746c67d2eafa3060ad2adb8043
Anton Mishanin <antonmishanin@mail.ru>
2021-12-13 23:15:20 +0700

    DCT-0018: move interface out of class

--- app/src/main/java/com/github/johnnysc/ecp/ui/dictionary/DictionaryUi.kt
+++ app/src/main/java/com/github/johnnysc/ecp/ui/dictionary/DictionaryUi.kt
@@ -1,8 +1,5 @@
 package com.github.johnnysc.ecp.ui.dictionary
 
-open class DictionaryUi(private val items: List<DictionaryItemUi>) {
+class DictionaryUi(private val items: List<DictionaryItemUi>)
 
-    interface DictionaryItemUi
-
-    class Base(items: List<DictionaryItemUi>) : DictionaryUi(items)
-}
+interface DictionaryItemUi
