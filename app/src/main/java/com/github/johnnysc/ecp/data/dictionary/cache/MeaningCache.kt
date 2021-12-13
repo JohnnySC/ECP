d205b569986ba45660c6cde0c8e956fdea75f01b
VlasovGleb <58880094+VlasovGleb@users.noreply.github.com>
2021-12-14 00:30:10 +0400

    Feature/dct 0011 meaning cache (#132)
    * DCT-0011 add interface MeaningCache with Base realization

---
+++ app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/MeaningCache.kt
@@ -0,0 +1,13 @@
+package com.github.johnnysc.ecp.data.dictionary.cache
+
+import com.github.johnnysc.ecp.data.dictionary.DefinitionCloud
+import io.realm.RealmList
+import io.realm.RealmObject
+
+interface MeaningCache {
+
+    open class Base : RealmObject(), MeaningCache {
+        var partOfSpeech: String = ""
+        var definitions: RealmList<DefinitionCloud> = RealmList()
+    }
+}
