ce2b28b140e5f78407887f1ce662b88d03047f4d
Oleksandr Putsenko <alexputsenko@ukr.net>
2021-12-14 23:27:47 +0200

    DCT-0021: moved Mapper interface into MeaningCache interface, used List instead of RealmList

--- app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/MeaningCache.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/MeaningCache.kt
@@ -4,15 +4,17 @@ import io.realm.RealmList
 import io.realm.RealmObject
 
 interface MeaningCache {
-    fun <T> map(mapper: BaseMeaningCache.Mapper<T>): T
+    fun <T> map(mapper: Mapper<T>): T
+
+    interface Mapper<T> {
+        fun map(partOfSpeech: String, definitions: List<BaseDefinitionCache>): T
+    }
 }
 
 open class BaseMeaningCache : RealmObject(), MeaningCache {
     var partOfSpeech: String = ""
     var definitions: RealmList<BaseDefinitionCache> = RealmList()
-    override fun <T> map(mapper: Mapper<T>) = mapper.map(partOfSpeech, definitions)
 
-    interface Mapper<T> {
-        fun map(partOfSpeech: String, definitions: RealmList<BaseDefinitionCache>): T
-    }
+    override fun <T> map(mapper: MeaningCache.Mapper<T>) = mapper.map(partOfSpeech, definitions)
+
 }
