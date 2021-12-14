099ab2e4431d5378f49c271e9452d224f10b34f6
evilstan <evilstan111@gmail.com>
2021-12-14 19:42:38 +0200

    DCT-0026: MeaningMapper interface and method added to MeaningCache interface,

--- app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/MeaningCache.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/MeaningCache.kt
@@ -3,9 +3,16 @@ package com.github.johnnysc.ecp.data.dictionary.cache
 import io.realm.RealmList
 import io.realm.RealmObject
 
-interface MeaningCache
+interface MeaningCache {
+    fun <T> map(mapper: BaseMeaningCache.Mapper<T>): T
+}
 
 open class BaseMeaningCache : RealmObject(), MeaningCache {
     var partOfSpeech: String = ""
     var definitions: RealmList<BaseDefinitionCache> = RealmList()
+    override fun <T> map(mapper: Mapper<T>) = mapper.map(partOfSpeech, definitions)
+
+    interface Mapper<T> {
+        fun map(partOfSpeech: String, definitions: RealmList<BaseDefinitionCache>): T
+    }
 }
