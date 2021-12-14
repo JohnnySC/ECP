03ceef873cc7b1feff63e571db9f8e82c78f97c3
Alexandr Putsenko <83665333+evilstan@users.noreply.github.com>
2021-12-14 19:51:18 +0200

    Dct 0021 definition mapper (#134)
    * DCT-0021:
    - new DefinitionMapper interface added
    - map() function to existing DefinitionCloud interface added
    
    * DCT-0021 nullable lists fixed, DefinitionMapper interface moved to DefinitionCloud
    
    * DCT-0021 Mapper interface renamed, nullable Lists fixed

--- app/src/main/java/com/github/johnnysc/ecp/data/dictionary/DefinitionCloud.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/dictionary/DefinitionCloud.kt
@@ -3,8 +3,9 @@ package com.github.johnnysc.ecp.data.dictionary
 import com.google.gson.annotations.SerializedName
 
 interface DefinitionCloud {
+    fun <T> map(mapper: Mapper<T>): T
 
-    data class Base (
+    data class Base(
         @SerializedName("definition")
         private val definition: String,
 
@@ -15,6 +16,24 @@ interface DefinitionCloud {
         private val synonyms: List<String>?,
 
         @SerializedName("antonyms")
-        private val antonyms: List<String>?
-    ) : DefinitionCloud
+        private val antonyms: List<String>?,
+    ) : DefinitionCloud {
+
+        override fun <T> map(mapper: Mapper<T>) =
+            mapper.map(
+                definition,
+                example,
+                synonyms ?: emptyList(),
+                antonyms ?: emptyList()
+            )
+    }
+
+    interface Mapper<T> {
+        fun map(
+            definition: String,
+            example: String,
+            synonyms: List<String>,
+            antonyms: List<String>
+        ): T
+    }
 }
