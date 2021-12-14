088dacc6562fef35e3ef932b2963f0f67ff1b837
rsTagirov <57265286+GDLock@users.noreply.github.com>
2021-12-14 14:02:09 +0500

    ECP-0068 DependencyContainer.Error added (#135)

--- app/src/main/java/com/github/johnnysc/ecp/data/core/DependencyContainer.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/DependencyContainer.kt
@@ -4,4 +4,11 @@ import androidx.lifecycle.ViewModel
 
 interface DependencyContainer {
     fun <T : ViewModel> module(clazz: Class<T>): BaseModule<T>
+
+    class Error : DependencyContainer {
+
+        override fun <T : ViewModel> module(clazz: Class<T>): BaseModule<T> {
+            throw IllegalStateException("no module found for $clazz")
+        }
+    }
 }
