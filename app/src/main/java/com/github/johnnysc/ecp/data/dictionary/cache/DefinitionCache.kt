cf90d63b490f53ecf5a3422401b3d67a56979921
VlasovGleb <58880094+VlasovGleb@users.noreply.github.com>
2021-12-14 22:21:02 +0400

    DCT-0025: DefinitionMapper (#146)
    * DCT-0025: DefinitionMapper

--- app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/DefinitionCache.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/DefinitionCache.kt
@@ -3,11 +3,25 @@ package com.github.johnnysc.ecp.data.dictionary.cache
 import io.realm.RealmList
 import io.realm.RealmObject
 
-interface DefinitionCache
+interface DefinitionCache {
+    fun <T> map(mapper: Mapper<T>): T
+
+    interface Mapper<T> {
+        fun map(
+            definition: String,
+            example: String,
+            synonyms: List<String>,
+            antonyms: List<String>
+        ): T
+    }
+}
 
 open class BaseDefinitionCache : RealmObject(), DefinitionCache {
     var definition: String = ""
     var example: String = ""
     var synonyms: RealmList<String> = RealmList()
     var antonyms: RealmList<String> = RealmList()
+
+    override fun <T> map(mapper: DefinitionCache.Mapper<T>) =
+        mapper.map(definition, example, synonyms, antonyms)
 }
