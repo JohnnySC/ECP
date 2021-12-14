84fbacea68d4f3cce5a9530fa0329503f8963d04
VlasovGleb <G.V.vlasov2078@gmail.com>
2021-12-14 20:28:14 +0400

    RealmObject вынесены из интерфейсов, Cloud вынесены из полей Cache.

--- app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/WordCache.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/WordCache.kt
@@ -3,7 +3,7 @@ package com.github.johnnysc.ecp.data.dictionary.cache
 import io.realm.RealmList
 import io.realm.RealmObject
 
-open class Base : RealmObject() {
+open class WordCache : RealmObject() {
     var word: String = ""
     var phonetics: RealmList<PhoneticCache> = RealmList()
     var meanings: RealmList<MeaningCache> = RealmList()
