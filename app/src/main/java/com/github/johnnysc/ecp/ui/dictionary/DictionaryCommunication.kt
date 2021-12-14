93b305345683c5b45d5575f93cefb3bc8306d4a8
Metamorfag <47786878+Metamorfag@users.noreply.github.com>
2021-12-14 12:48:55 +0200

    DCT-0017: DictionaryCommunication - fix DictionaryCommunication interface and implementation

--- app/src/main/java/com/github/johnnysc/ecp/ui/dictionary/DictionaryCommunication.kt
+++ app/src/main/java/com/github/johnnysc/ecp/ui/dictionary/DictionaryCommunication.kt
@@ -2,6 +2,6 @@ package com.github.johnnysc.ecp.ui.dictionary
 
 import com.github.johnnysc.ecp.ui.core.Communication
 
-interface DictionaryCommunication : Communication<List<DictionaryItemUi>> {
-    class Base : Communication.Base<List<DictionaryItemUi>>(), DictionaryCommunication
+interface DictionaryCommunication : Communication<DictionaryUi> {
+    class Base : Communication.Base<DictionaryUi>(), DictionaryCommunication
 }
