db95a1d44a017a48cd78fa583e572b67f2dd86d7
VlasovGleb <58880094+VlasovGleb@users.noreply.github.com>
2021-12-14 21:30:39 +0400

    RealmObject вынесены из интерфейсов. (#142)

--- app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/DefinitionCache.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/DefinitionCache.kt
@@ -3,12 +3,11 @@ package com.github.johnnysc.ecp.data.dictionary.cache
 import io.realm.RealmList
 import io.realm.RealmObject
 
-interface DefinitionCache {
+interface DefinitionCache
 
-    open class Base : RealmObject(), DefinitionCache {
-        var definition: String = ""
-        var example: String = ""
-        var synonyms: RealmList<String> = RealmList()
-        var antonyms: RealmList<String> = RealmList()
-    }
+open class BaseDefinitionCache : RealmObject(), DefinitionCache {
+    var definition: String = ""
+    var example: String = ""
+    var synonyms: RealmList<String> = RealmList()
+    var antonyms: RealmList<String> = RealmList()
 }
--- app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/WordCache.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/WordCache.kt
@@ -1,15 +1,12 @@
 package com.github.johnnysc.ecp.data.dictionary.cache
 
-import com.github.johnnysc.ecp.data.dictionary.MeaningCloud
-import com.github.johnnysc.ecp.data.dictionary.PhoneticCloud
 import io.realm.RealmList
 import io.realm.RealmObject
 
-interface WordCache {
+interface WordCache
 
-    open class Base : RealmObject(), WordCache {
-        var word: String = ""
-        var phonetics: RealmList<PhoneticCloud> = RealmList()
-        var meanings: RealmList<MeaningCloud> = RealmList()
-    }
+open class BaseWordCache : RealmObject(), WordCache {
+    var word: String = ""
+    var phonetics: RealmList<BasePhoneticCache> = RealmList()
+    var meanings: RealmList<BaseMeaningCache> = RealmList()
 }
