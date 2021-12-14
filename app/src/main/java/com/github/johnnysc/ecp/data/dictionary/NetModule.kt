0d242e711386a80cfcd44c1040ece08549fab4cd
Metamorfag <47786878+Metamorfag@users.noreply.github.com>
2021-12-14 11:36:42 +0200

    DCT-0009: MakeService - added NetModule class

--- app/src/main/java/com/github/johnnysc/ecp/data/dictionary/NetModule.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/dictionary/NetModule.kt
@@ -3,19 +3,13 @@ package com.github.johnnysc.ecp.data.dictionary
 import com.github.johnnysc.ecp.data.core.MakeService
 import com.github.johnnysc.ecp.data.core.ProvideInterceptor
 import com.github.johnnysc.ecp.data.core.ProvideOkHttpClientBuilder
-import retrofit2.Retrofit
+import com.github.johnnysc.ecp.data.core.ProvideRetrofit
 
-class NetModule(
-    clientBuilder: ProvideOkHttpClientBuilder,
-    interceptor: ProvideInterceptor
-) : MakeService {
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .client(
-            clientBuilder.okHttpClientBuilder()
-                .addInterceptor(interceptor.interceptor()).build()
-        )
-        .build()
+class NetModule(isDebug: Boolean) : MakeService {
+    private val retrofit = ProvideRetrofit.Base(
+        BASE_URL,
+        ProvideOkHttpClientBuilder.Base(ProvideInterceptor.Base(isDebug).interceptor())
+    ).retrofit()
 
     override fun <T> service(clazz: Class<T>): T = retrofit.create(clazz)
 
