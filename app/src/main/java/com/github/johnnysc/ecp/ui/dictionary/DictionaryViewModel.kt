57b8575ffa0e1849faa863d9d1a655fd10a4493b
Metamorfag <47786878+Metamorfag@users.noreply.github.com>
2021-12-15 14:16:11 +0200

    DCT-0016: ViewModel - added DictionaryViewModel

---
+++ app/src/main/java/com/github/johnnysc/ecp/ui/dictionary/DictionaryViewModel.kt
@@ -0,0 +1,18 @@
+package com.github.johnnysc.ecp.ui.dictionary
+
+import com.github.johnnysc.ecp.data.core.BaseViewModel
+import com.github.johnnysc.ecp.data.core.SearchWord
+import com.github.johnnysc.ecp.ui.core.NavigationCommunication
+import com.github.johnnysc.ecp.ui.core.NavigationUi
+
+class DictionaryViewModel(
+    private val navigation: NavigationCommunication,
+    dictionaryCommunication: DictionaryCommunication
+) : BaseViewModel<DictionaryUi>(dictionaryCommunication), SearchWord {
+
+    fun init() {
+        navigation.map(NavigationUi())
+    }
+
+    override fun searchWord(word: String) {}
+}
