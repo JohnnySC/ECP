6560f2e51722eaa485ca01e87e0b6c73c6376c03
Anton Mishanin <antonmishanin@mail.ru>
2021-12-15 22:09:43 +0700

    DCT-0027: create phonetic cache mapper

--- app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/PhoneticCache.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/PhoneticCache.kt
@@ -2,9 +2,20 @@ package com.github.johnnysc.ecp.data.dictionary.cache
 
 import io.realm.RealmObject
 
-interface PhoneticCache
+interface PhoneticCache {
+    fun <T> map(mapper: Mapper<T>): T
+
+    interface Mapper<T> {
+        fun map(
+            text: String,
+            audio: String
+        ): T
+    }
+}
 
 open class BasePhoneticCache : RealmObject(), PhoneticCache {
     var text: String = ""
     var audio: String = ""
+
+    override fun <T> map(mapper: PhoneticCache.Mapper<T>) = mapper.map(text, audio)
 }
