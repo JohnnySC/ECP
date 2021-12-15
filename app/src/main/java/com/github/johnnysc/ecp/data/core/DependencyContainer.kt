09c5d731d42a255e4b82261eda3581f50b3134cb
Белова Елизавета Павловна <EPBelova.SBT@sberbank.ru>
2021-12-15 22:53:40 +0300

    ECP-0063 added DependencyContainer.Main

--- app/src/main/java/com/github/johnnysc/ecp/data/core/DependencyContainer.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/DependencyContainer.kt
@@ -1,6 +1,9 @@
 package com.github.johnnysc.ecp.data.core
 
 import androidx.lifecycle.ViewModel
+import com.github.johnnysc.ecp.core.CoreModule
+import com.github.johnnysc.ecp.ui.core.MainModule
+import com.github.johnnysc.ecp.ui.core.MainViewModel
 
 interface DependencyContainer {
     fun <T : ViewModel> module(clazz: Class<T>): BaseModule<T>
@@ -10,5 +13,14 @@ interface DependencyContainer {
         override fun <T : ViewModel> module(clazz: Class<T>): BaseModule<T> {
             throw IllegalStateException("no module found for $clazz")
         }
+    }
+
+    class Main(private val dependencyContainer: DependencyContainer) : DependencyContainer {
+
+        override fun <T : ViewModel> module(clazz: Class<T>): BaseModule<T> =
+            when (clazz) {
+                MainViewModel::class.java -> MainModule(CoreModule()).viewModel() as BaseModule<T>
+                else -> dependencyContainer.module(clazz)
+            }
     }
 }
