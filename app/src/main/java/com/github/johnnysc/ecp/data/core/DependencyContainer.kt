2ce04ee7608b5073b10620a5daa2fde53c074aee
Artem <androidmen2000@gmail.com>
2021-12-15 15:56:13 +0200

    ECP-0063: Add else branch

--- app/src/main/java/com/github/johnnysc/ecp/data/core/DependencyContainer.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/core/DependencyContainer.kt
@@ -3,6 +3,7 @@ package com.github.johnnysc.ecp.data.core
 import androidx.lifecycle.ViewModel
 import com.github.johnnysc.ecp.core.CoreModule
 import com.github.johnnysc.ecp.ui.core.MainModule
+import com.github.johnnysc.ecp.ui.core.MainViewModel
 
 interface DependencyContainer {
     fun <T : ViewModel> module(clazz: Class<T>): BaseModule<T>
@@ -10,7 +11,8 @@ interface DependencyContainer {
     class Main(private val coreModule: CoreModule) : DependencyContainer {
         override fun <T : ViewModel> module(clazz: Class<T>): BaseModule<T> =
             when (clazz::class.java) {
-                else -> MainModule(coreModule = coreModule) as BaseModule<T>
+                MainViewModel::class.java -> MainModule(coreModule = coreModule) as BaseModule<T>
+                else -> throw IllegalStateException("no module found for $clazz")
             }
     }
 
