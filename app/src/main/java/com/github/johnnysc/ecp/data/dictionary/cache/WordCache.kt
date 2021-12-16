c317c2d8affcbc528ce41fab3b3249085edcd364
Artem <androidmen2000@gmail.com>
2021-12-16 16:42:05 +0200

    DCT-0028: Create mapper. Add map function

--- app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/WordCache.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/dictionary/cache/WordCache.kt
@@ -3,10 +3,22 @@ package com.github.johnnysc.ecp.data.dictionary.cache
 import io.realm.RealmList
 import io.realm.RealmObject
 
-interface WordCache
+interface WordCache {
+    fun <T> map(mapper: Mapper<T>): T
+
+    interface Mapper<T> {
+        fun map(
+            word: String,
+            phonetics: RealmList<BasePhoneticCache>,
+            meanings: RealmList<BaseMeaningCache>
+        ): T
+    }
+}
 
 open class BaseWordCache : RealmObject(), WordCache {
     var word: String = ""
     var phonetics: RealmList<BasePhoneticCache> = RealmList()
     var meanings: RealmList<BaseMeaningCache> = RealmList()
+
+    override fun <T> map(mapper: WordCache.Mapper<T>) = mapper.map(word, phonetics, meanings)
 }
