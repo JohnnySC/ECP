9fb552f47d25856b3fe6c93999bc141ed7b56bc5
GDLock <ESK.GD.Lock@gmail.com>
2021-12-18 18:03:28 +0500

    ECP-064 ViewModelsFactory in App.kt

--- app/src/main/java/com/github/johnnysc/ecp/App.kt
+++ app/src/main/java/com/github/johnnysc/ecp/App.kt
@@ -3,9 +3,25 @@ package com.github.johnnysc.ecp
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelStoreOwner
+import com.github.johnnysc.ecp.core.CoreModule
 import com.github.johnnysc.ecp.core.ProvideViewModel
+import com.github.johnnysc.ecp.data.core.DependencyContainer
+import com.github.johnnysc.ecp.ui.core.ViewModelsFactory
 
 class App : Application(), ProvideViewModel {
+
+    private val coreModule = CoreModule()
+
+    private val factory by lazy {
+        ViewModelsFactory(
+            DependencyContainer.Base(
+                DependencyContainer.Main(
+                    DependencyContainer.Error(),
+                    coreModule
+                ), coreModule
+            )
+        )
+    }
 
     override fun <T : ViewModel> provideViewModel(
         clazz: Class<T>,
