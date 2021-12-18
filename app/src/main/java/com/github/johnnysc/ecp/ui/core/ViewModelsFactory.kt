3ca2f57abacdf0f444f60f4f8c046180c5f85bff
Dima Variukhin <47786878+Dima-Variukhin@users.noreply.github.com>
2021-12-18 10:05:36 +0200

    DCT-0020: DictionaryModule - added DictionaryModule (#159)
    Co-authored-by: Metamorfag <47786878+Metamorfag@users.noreply.github.com>

--- app/src/main/java/com/github/johnnysc/ecp/ui/core/ViewModelsFactory.kt
+++ app/src/main/java/com/github/johnnysc/ecp/ui/core/ViewModelsFactory.kt
@@ -4,8 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.github.johnnysc.ecp.data.core.DependencyContainer
 
-class ViewModelsFactory(private val dependencyContainer: DependencyContainer) : ViewModelProvider.Factory {
+class ViewModelsFactory(private val dependencyContainer: DependencyContainer) :
+    ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T =
-        dependencyContainer.module(modelClass).viewModel()
+        dependencyContainer.module(modelClass).viewModel() as T
 }
