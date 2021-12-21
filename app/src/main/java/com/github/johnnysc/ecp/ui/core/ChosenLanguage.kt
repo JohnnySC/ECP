f8eaf2236f26fcc1e865521fa97b3a15c0c116c4
Metamorfag <47786878+Metamorfag@users.noreply.github.com>
2021-12-21 15:05:59 +0200

    DCT-0027 ChosenLanguage - fix ChosenLanguage interface and implementation

--- app/src/main/java/com/github/johnnysc/ecp/ui/core/ChosenLanguage.kt
+++ app/src/main/java/com/github/johnnysc/ecp/ui/core/ChosenLanguage.kt
@@ -1,22 +1,17 @@
 package com.github.johnnysc.ecp.ui.core
 
-import com.github.johnnysc.ecp.core.ProvidePreferences
 import com.github.johnnysc.ecp.core.Save
 import com.github.johnnysc.ecp.data.core.PreferenceDataSource
 import com.github.johnnysc.ecp.data.core.Read
 
 interface ChosenLanguage : Read<String>, Save<String> {
-    class Base(providePreferences: ProvidePreferences) : ChosenLanguage {
-
-        private val sharedPreferences = providePreferences.providePreferences(FILE_NAME)
-        private val preferenceDataSource = PreferenceDataSource.Base(sharedPreferences)
+    class Base(private val preferenceDataSource: PreferenceDataSource) : ChosenLanguage {
 
         override fun save(data: String) = preferenceDataSource.save(KEY, data)
         override fun read() = preferenceDataSource.read(KEY, "en")
 
-        private companion object {
-            const val FILE_NAME = "chosenLanguageFileName"
-            const val KEY = "chosenLanguageKey"
+        companion object {
+            private const val KEY = "chosenLanguageKey"
         }
     }
 }
