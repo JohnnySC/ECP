de54cc77a905391d52741a67c5c4c99c17219485
Dima Variukhin <47786878+Dima-Variukhin@users.noreply.github.com>
2021-12-21 19:42:05 +0200

    DCT-0027 ChosenLanguage - added ChosenLanguage interface and implementation (#169)
    Co-authored-by: Metamorfag <47786878+Metamorfag@users.noreply.github.com>

---
+++ app/src/main/java/com/github/johnnysc/ecp/data/dictionary/ChosenLanguage.kt
@@ -0,0 +1,17 @@
+package com.github.johnnysc.ecp.data.dictionary
+
+import com.github.johnnysc.ecp.core.Save
+import com.github.johnnysc.ecp.data.core.PreferenceDataSource
+import com.github.johnnysc.ecp.data.core.Read
+
+interface ChosenLanguage : Read<String>, Save<String> {
+    class Base(private val preferenceDataSource: PreferenceDataSource) : ChosenLanguage {
+
+        override fun save(data: String) = preferenceDataSource.save(KEY, data)
+        override fun read() = preferenceDataSource.read(KEY, "en")
+
+        companion object {
+            private const val KEY = "chosenLanguageKey"
+        }
+    }
+}
