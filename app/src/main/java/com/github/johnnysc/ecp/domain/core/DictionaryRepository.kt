e163151c04f7571ecfd00cc8057b7c4df2d39b60
Белова Елизавета Павловна <EPBelova.SBT@sberbank.ru>
2021-12-24 22:37:48 +0300

    DCT-0023: languages removed

--- app/src/main/java/com/github/johnnysc/ecp/domain/core/DictionaryRepository.kt
+++ app/src/main/java/com/github/johnnysc/ecp/domain/core/DictionaryRepository.kt
@@ -8,13 +8,3 @@ interface DictionaryRepository {
     fun changeLanguage(value: String)
     suspend fun searchWord(word: String): List<WordData>
 }
-
-enum class Language {
-    en,
-    es,
-    fr,
-    ja,
-    ru,
-    de,
-    tr
-}
