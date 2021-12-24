e4c56989fa195a2f4d7e8b85c62f0abdb196658f
Белова Елизавета Павловна <EPBelova.SBT@sberbank.ru>
2021-12-24 22:25:18 +0300

    DCT-0023: one more fixed method

--- app/src/main/java/com/github/johnnysc/ecp/domain/core/DictionaryRepository.kt
+++ app/src/main/java/com/github/johnnysc/ecp/domain/core/DictionaryRepository.kt
@@ -1,13 +1,12 @@
 package com.github.johnnysc.ecp.domain.core
 
-import com.github.johnnysc.ecp.data.dictionary.ChosenLanguage
 import com.github.johnnysc.ecp.data.dictionary.WordData
 
 interface DictionaryRepository {
 
-    fun chosenLanguage(): ChosenLanguage
+    fun chosenLanguage(): String
     fun changeLanguage(value: String)
-    suspend fun searchWord(word: String): List<WordData>
+    suspend fun searchWord(language: String, word: String): List<WordData>
 }
 
 enum class Language {
