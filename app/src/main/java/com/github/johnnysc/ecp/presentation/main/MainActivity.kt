7a8a9c828a32aa7c7338e1c09a01946cad5f5b3d
misha <mishafuze10@gmail.com>
2022-06-28 18:12:42 +0600

    2

--- app/src/main/java/com/github/johnnysc/ecp/presentation/main/MainActivity.kt
+++ app/src/main/java/com/github/johnnysc/ecp/presentation/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.johnnysc.ecp
+package com.github.johnnysc.ecp.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.johnnysc.ecp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
