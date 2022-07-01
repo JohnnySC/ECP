001bd9fb205ba3c322f03909b255c43b314ffc29
Anton Mishanin <antonmishanin@mail.ru>
2022-07-01 22:12:12 +0700

    0004: messages communication, ItemUi -> MessageUi

--- app/src/main/java/com/github/johnnysc/ecp/presentation/messages/MessagesViewModel.kt
+++ app/src/main/java/com/github/johnnysc/ecp/presentation/messages/MessagesViewModel.kt
@@ -3,11 +3,10 @@ package com.github.johnnysc.ecp.presentation.messages
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
-import com.github.johnnysc.coremvvm.presentation.adapter.ItemUi
 
 class MessagesViewModel : ViewModel() {
 
-    fun observe(owner: LifecycleOwner, observer: Observer<List<ItemUi>>) {
+    fun observe(owner: LifecycleOwner, observer: Observer<List<MessageUI>>) {
 
     }
 }
