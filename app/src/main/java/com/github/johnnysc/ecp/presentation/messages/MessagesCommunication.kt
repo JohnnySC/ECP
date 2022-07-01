90d29fc071dd2cd9d3129eab8f6c168c21d63708
Anton Mishanin <antonmishanin@mail.ru>
2022-07-01 21:51:29 +0700

    0004: messages communication, extends to interface

--- app/src/main/java/com/github/johnnysc/ecp/presentation/messages/MessagesCommunication.kt
+++ app/src/main/java/com/github/johnnysc/ecp/presentation/messages/MessagesCommunication.kt
@@ -6,17 +6,21 @@ import androidx.lifecycle.Observer
 import com.github.johnnysc.coremvvm.presentation.Communication
 import com.github.johnnysc.coremvvm.presentation.adapter.ItemUi
 
-class MessagesCommunication(
-    private val mutableLiveData: MutableLiveData<List<ItemUi>> = MutableLiveData()
-) : Communication.Observe<List<ItemUi>>, Communication.Update<ItemUi> {
+interface MessagesCommunication : Communication.Observe<List<ItemUi>>,
+    Communication.Update<ItemUi> {
 
-    override fun observe(owner: LifecycleOwner, observer: Observer<List<ItemUi>>) =
-        mutableLiveData.observe(owner, observer)
+    class Base(
+        private val mutableLiveData: MutableLiveData<List<ItemUi>>
+    ) : MessagesCommunication {
 
-    override fun map(data: ItemUi) {
-        val result = mutableListOf<ItemUi>()
-        mutableLiveData.value?.let(result::addAll)
-        result.add(data)
-        mutableLiveData.value = result
+        override fun observe(owner: LifecycleOwner, observer: Observer<List<ItemUi>>) =
+            mutableLiveData.observe(owner, observer)
+
+        override fun map(data: ItemUi) {
+            val result = mutableListOf<ItemUi>()
+            mutableLiveData.value?.let(result::addAll)
+            result.add(data)
+            mutableLiveData.value = result
+        }
     }
 }
