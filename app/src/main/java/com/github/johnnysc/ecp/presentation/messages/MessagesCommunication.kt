970fe8c02a5d28f9d70a1c1559f0bf1d6c5ef8f4
Anton Mishanin <antonmishanin@mail.ru>
2022-07-01 21:32:41 +0700

    0004: messages communication, remove unused interface

--- app/src/main/java/com/github/johnnysc/ecp/presentation/messages/MessagesCommunication.kt
+++ app/src/main/java/com/github/johnnysc/ecp/presentation/messages/MessagesCommunication.kt
@@ -6,20 +6,17 @@ import androidx.lifecycle.Observer
 import com.github.johnnysc.coremvvm.presentation.Communication
 import com.github.johnnysc.coremvvm.presentation.adapter.ItemUi
 
-interface MessagesCommunication {
+class MessagesCommunication(
+    private val mutableLiveData: MutableLiveData<List<ItemUi>> = MutableLiveData()
+) : Communication.Observe<List<ItemUi>>, Communication.Update<ItemUi> {
 
-    class Base(
-        private val mutableLiveData: MutableLiveData<List<ItemUi>> = MutableLiveData()
-    ) : MessagesCommunication, Communication.Observe<List<ItemUi>>, Communication.Update<ItemUi> {
+    override fun observe(owner: LifecycleOwner, observer: Observer<List<ItemUi>>) =
+        mutableLiveData.observe(owner, observer)
 
-        override fun observe(owner: LifecycleOwner, observer: Observer<List<ItemUi>>) =
-            mutableLiveData.observe(owner, observer)
-
-        override fun map(data: ItemUi) {
-            val result = mutableListOf<ItemUi>()
-            mutableLiveData.value?.let(result::addAll)
-            result.add(data)
-            mutableLiveData.value = result
-        }
+    override fun map(data: ItemUi) {
+        val result = mutableListOf<ItemUi>()
+        mutableLiveData.value?.let(result::addAll)
+        result.add(data)
+        mutableLiveData.value = result
     }
 }
