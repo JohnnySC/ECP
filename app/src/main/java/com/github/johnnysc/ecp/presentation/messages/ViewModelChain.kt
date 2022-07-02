8ee5e541411847768ed82a8b449525642a6000d0
misha <mishafuze10@gmail.com>
2022-07-02 15:06:33 +0600

    ChangeViewModelChain implementation

--- app/src/main/java/com/github/johnnysc/ecp/presentation/messages/ViewModelChain.kt
+++ app/src/main/java/com/github/johnnysc/ecp/presentation/messages/ViewModelChain.kt
@@ -1,48 +1,23 @@
 package com.github.johnnysc.ecp.presentation.messages
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.github.johnnysc.coremvvm.core.Dispatchers
 
 abstract class ViewModelChain(
     private val featureChain: FeatureChain.CheckAndHandle,
-    private val dispatchers: Dispatchers,
-    private val messagesCommunication: MessagesCommunication.Update
-) : ViewModel(), FeatureChain.Check {
-    var nextFeatureChain: MutableList<FeatureChain.CheckAndHandle> = mutableListOf()
+) : FeatureChain.Handle {
+    private lateinit var nextFeatureChain: FeatureChain.Handle
 
-    override fun canHandle(message: String): Boolean {
-        var result = false
+    override suspend fun handle(message: String): MessageUI {
+        var result: MessageUI
         if (featureChain.canHandle(message)) {
-            result = true
-            handleMessage(featureChain, message)
+            result = featureChain.handle(message)
         } else {
-            for (featureChain in nextFeatureChain) {
-                if (featureChain.canHandle(message)) {
-                    handleMessage(featureChain, message)
-                    result = true
-                    break
-                }
-            }
+            result = nextFeatureChain.handle(message)
         }
         return result
     }
 
-    private fun handleResult(messageUI: MessageUI) {
-        messagesCommunication.map(messageUI)
-    }
-
-    fun handleMessage(featureChain: FeatureChain.CheckAndHandle, message: String) {
-        dispatchers.launchBackground(viewModelScope)
-        {
-            val messageResult = featureChain.handle(message)
-            dispatchers.changeToUI {
-                handleResult(messageResult)
-            }
-        }
-    }
-
-    fun setFeatureChain(featureChain: FeatureChain.CheckAndHandle) {
-        nextFeatureChain.add(featureChain)
+    fun setNextFeatureChain(featureChain: FeatureChain.CheckAndHandle) {
+        nextFeatureChain = featureChain
     }
 }
