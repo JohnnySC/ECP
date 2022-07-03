203eac7face6707dcbeef457f589b08b1cdb7967
misha <mishafuze10@gmail.com>
2022-07-03 21:56:22 +0600

    0011- before push

--- app/src/main/java/com/github/johnnysc/ecp/sl/message/MessagesModule.kt
+++ app/src/main/java/com/github/johnnysc/ecp/sl/message/MessagesModule.kt
@@ -1,21 +1,11 @@
 package com.github.johnnysc.ecp.sl.message
 
-import com.github.johnnysc.coremvvm.core.Dispatchers
 import com.github.johnnysc.coremvvm.sl.Module
-import com.github.johnnysc.coremvvm.sl.ProvideViewModel
-import com.github.johnnysc.ecp.presentation.messages.MessagesCommunication
 import com.github.johnnysc.ecp.presentation.messages.MessagesViewModel
-import com.github.johnnysc.ecp.presentation.messages.ViewModelChain
-import com.github.johnnysc.ecp.sl.ProvideViewModelChain
 
 class MessagesModule(
 
 ) : Module<MessagesViewModel> {
 
-    override fun viewModel(): MessagesViewModel {
-
-        return MessagesViewModel(
-
-        )
-    }
+    override fun viewModel() = MessagesViewModel()
 }
