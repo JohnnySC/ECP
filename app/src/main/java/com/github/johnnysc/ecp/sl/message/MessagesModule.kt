7dd705d4a23d07d406f024df28a32374d2ee1986
misha <mishafuze10@gmail.com>
2022-07-03 19:43:51 +0600

    0011- middle, need to megh

--- app/src/main/java/com/github/johnnysc/ecp/sl/message/MessagesModule.kt
+++ app/src/main/java/com/github/johnnysc/ecp/sl/message/MessagesModule.kt
@@ -1,11 +1,27 @@
 package com.github.johnnysc.ecp.sl.message
 
 import com.github.johnnysc.coremvvm.sl.Module
+import com.github.johnnysc.coremvvm.sl.ProvideViewModel
+import com.github.johnnysc.ecp.presentation.messages.MessagesCommunication
 import com.github.johnnysc.ecp.presentation.messages.MessagesViewModel
+import com.github.johnnysc.ecp.presentation.messages.ViewModelChain
+import com.github.johnnysc.ecp.sl.ProvideViewModelChain
 
 class MessagesModule(
 
 ) : Module<MessagesViewModel> {
 
-    override fun viewModel() = MessagesViewModel()
+    override fun viewModel(): MessagesViewModel {
+        val listOfViewModelChain = providesViewModelChain.map { provideViewModelChain ->
+            provideViewModelChain.viewModelChain()
+        }
+
+        val finalChain = listOfViewModelChain.first()
+
+        listOfViewModelChain.forEachIndexed { index, viewModelChain ->
+            if (index < listOfViewModelChain.lastIndex)
+                listOfViewModelChain[index].setNextFeatureChain(listOfViewModelChain[index + 1])
+        }
+        return MessagesViewModel(messagesCommunication,finalChain)
+    }
 }
