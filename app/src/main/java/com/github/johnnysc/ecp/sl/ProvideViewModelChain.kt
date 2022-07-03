b427910767c54454c94d4624056c27a85cee2c56
misha <mishafuze10@gmail.com>
2022-07-03 14:37:37 +0600

    0008- changed parameters in viewModel chain in interface ProvideViewModelChain.kt

--- app/src/main/java/com/github/johnnysc/ecp/sl/ProvideViewModelChain.kt
+++ app/src/main/java/com/github/johnnysc/ecp/sl/ProvideViewModelChain.kt
@@ -1,8 +1,8 @@
-package com.github.johnnysc.ecp.sl.view_model_chain
+package com.github.johnnysc.ecp.sl
 
 import com.github.johnnysc.ecp.presentation.messages.ViewModelChain
 
 interface ProvideViewModelChain<T : ViewModelChain> {
-    fun viewModelChain(): T
 
+    fun viewModelChain(): T
 }
