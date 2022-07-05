a518baaf433b847f166c4b85fc0bb5219223fc2b
velvet <berhram@protonmail.com>
2022-07-05 13:49:47 +0300

    0012: remove todo

--- app/src/main/java/com/github/johnnysc/ecp/presentation/messages/adapter/MessageAdapter.kt
+++ app/src/main/java/com/github/johnnysc/ecp/presentation/messages/adapter/MessageAdapter.kt
@@ -4,7 +4,6 @@ import com.github.johnnysc.coremvvm.presentation.adapter.GenericAdapter
 import com.github.johnnysc.coremvvm.presentation.adapter.ViewHolderFactoryChain
 import com.github.johnnysc.ecp.presentation.messages.MessageUI
 
-//TODO remove redundant interface
 interface MessageAdapter {
     class Base : GenericAdapter<MessageUI>(
         CorrectAiMessageViewHolderFactoryChain(
