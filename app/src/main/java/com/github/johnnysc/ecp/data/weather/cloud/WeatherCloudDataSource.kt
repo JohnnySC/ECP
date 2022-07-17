46119ef6ace2d8e022b7301d85e54fb832e7223c
misha <mishafuze10@gmail.com>
2022-07-17 15:36:47 +0600

    add unknown host exception instead ThereIsNoConnection in interactor

--- app/src/main/java/com/github/johnnysc/ecp/data/weather/cloud/WeatherCloudDataSource.kt
+++ app/src/main/java/com/github/johnnysc/ecp/data/weather/cloud/WeatherCloudDataSource.kt
@@ -1,7 +1,18 @@
 package com.github.johnnysc.ecp.data.weather.cloud
 
+import android.content.Context
 import com.github.johnnysc.coremvvm.data.CloudDataSource
 import com.github.johnnysc.coremvvm.data.HandleError
+import com.github.johnnysc.ecp.R
+import com.github.johnnysc.ecp.core.RawResourceReader
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
+import retrofit2.HttpException
+import retrofit2.Response
+import java.net.UnknownHostException
 
 interface WeatherCloudDataSource {
 
