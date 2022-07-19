3ca8b475dcd4ef4af0d50a7ff68ab5e2e919771c
misha <mishafuze10@gmail.com>
2022-07-19 19:09:32 +0600

    remove buildConfig constants

--- app/src/main/java/com/github/johnnysc/ecp/sl/weather/ProvideCityPreferenceDataStore.kt
+++ app/src/main/java/com/github/johnnysc/ecp/sl/weather/ProvideCityPreferenceDataStore.kt
@@ -1,7 +1,6 @@
 package com.github.johnnysc.ecp.sl.weather
 
 import com.github.johnnysc.coremvvm.sl.CoreModule
-import com.github.johnnysc.ecp.BuildConfig.USE_MOCKS
 import com.github.johnnysc.ecp.data.weather.cache.CityPreferenceDataStore
 
 interface ProvideCityPreferenceDataStore {
@@ -10,6 +9,7 @@ interface ProvideCityPreferenceDataStore {
 
     class Base(private val coreModule: CoreModule) : ProvideCityPreferenceDataStore {
         private val citySharedPrefsKey = "city_prefs"
-        override fun provideCityPreferenceDataStore()= CityPreferenceDataStore.Base(coreModule.sharedPreferences(citySharedPrefsKey))
+        override fun provideCityPreferenceDataStore() =
+            CityPreferenceDataStore.Base(coreModule.sharedPreferences(citySharedPrefsKey))
     }
 }
--- app/src/main/java/com/github/johnnysc/ecp/sl/weather/ProvideWeatherViewModelChain.kt
+++ app/src/main/java/com/github/johnnysc/ecp/sl/weather/ProvideWeatherViewModelChain.kt
@@ -1,12 +1,9 @@
 package com.github.johnnysc.ecp.sl.weather
 
 import android.content.Context
-
 import com.github.johnnysc.coremvvm.core.ManageResources
 import com.github.johnnysc.coremvvm.sl.CoreModule
-import com.github.johnnysc.ecp.BuildConfig.USE_MOCKS
-
-import com.github.johnnysc.ecp.data.weather.cache.CityPreferenceDataStore
+import com.github.johnnysc.ecp.BuildConfig.BUILD_TYPE
 import com.github.johnnysc.ecp.data.weather.cloud.WeatherCloudDataSource
 import com.github.johnnysc.ecp.presentation.weather.WeatherChain
 import com.github.johnnysc.ecp.presentation.weather.WeatherViewModelChain
@@ -19,10 +16,12 @@ class ProvideWeatherViewModelChain(
     private val context: Context
 ) : ProvideViewModelChain<WeatherViewModelChain> {
 
+    private val uiTestVariant="uitests"
+
     override fun viewModelChain(): WeatherViewModelChain {
         val manageResources = ManageResources.Base(context)
         val provideWeatherCloudDataSource =
-            if (USE_MOCKS) {
+            if (BUILD_TYPE==uiTestVariant) {
                 val mockedRes = WeatherCloudDataSource.MockData(context)
                 ProvideWeatherCloudDataSource.Mock(Gson(), mockedRes,coreModule)
             } else {
