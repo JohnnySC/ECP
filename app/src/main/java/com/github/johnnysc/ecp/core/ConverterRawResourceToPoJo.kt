503530d8417dac6912475360d154cde54a45b82f
misha <mishafuze10@gmail.com>
2022-07-21 21:37:55 +0600

    remove buildConfig constants

--- app/src/main/java/com/github/johnnysc/ecp/core/ConverterRawResourceToPoJo.kt
+++ app/src/main/java/com/github/johnnysc/ecp/core/ConverterRawResourceToPoJo.kt
@@ -6,11 +6,13 @@ abstract class ConverterRawResourceToPoJo<O,W>(
     private val readRawResource: ReadRawResource,
     private val converter: Converter<O>
 ) {
+
     fun convert(@RawRes id:Int):W
     {
         val resource=readRawResource.readText(id)
         return wrapResult(converter.convert(resource))
     }
+
     protected abstract fun wrapResult(result: O):W
 
 }
