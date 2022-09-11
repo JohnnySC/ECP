1a0db0eac8343ff0d37155ab8734bb32429fd051
velvet <berhram@protonmail.com>
2022-09-11 23:18:29 +0300

    fix tests

--- app/src/main/java/com/github/johnnysc/ecp/schedule/presentation/commands/ConvertTime.kt
+++ app/src/main/java/com/github/johnnysc/ecp/schedule/presentation/commands/ConvertTime.kt
@@ -1,6 +1,10 @@
 package com.github.johnnysc.ecp.schedule.presentation.commands
 
 import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 interface ConvertTime {
@@ -11,11 +15,11 @@ interface ConvertTime {
 
     class Base(
         private val dateFormat: SimpleDateFormat =
-            SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).
+            SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
     ) : ConvertTime {
 
         override fun fromTimeToString(time: Long): String =
-            dateFormat.format(Date(time))
+            dateFormat.format(time)
 
         override fun fromStringToTime(string: String): Long =
             dateFormat.parse(string)?.time!!
--- app/src/test/java/com/github/johnnysc/ecp/schedule/presentation/commands/ConvertTimeTest.kt
+++ app/src/test/java/com/github/johnnysc/ecp/schedule/presentation/commands/ConvertTimeTest.kt
@@ -2,18 +2,40 @@ package com.github.johnnysc.ecp.schedule.presentation.commands
 
 import org.junit.Assert
 import org.junit.Test
+import java.text.SimpleDateFormat
+import java.util.*
 
 internal class ConvertTimeTest {
 
     @Test
-    fun `success from long to string format with proper long`() {
-        val convertTime = ConvertTime.Base()
-        Assert.assertEquals("11.09.2022", convertTime.fromTimeToString(	1662888445000))
+    fun `success from long to string format with`() {
+        val simpleDateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+        simpleDateFormat.timeZone = TimeZone.getTimeZone("Europe/Moscow")
+        val convertTime = ConvertTime.Base(simpleDateFormat)
+        Assert.assertEquals("11.09.2022", convertTime.fromTimeToString(		1662843600000))
     }
 
     @Test
-    fun `success from long to string format with proper long`() {
-        val convertTime = ConvertTime.Base()
-        Assert.assertEquals("11.09.2022", convertTime.fromTimeToString(	1662888445000))
+    fun `success from same long but to different string due to timezone`() {
+        val simpleDateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+        simpleDateFormat.timeZone = TimeZone.getTimeZone("America/Denver")
+        val convertTime = ConvertTime.Base(simpleDateFormat)
+        Assert.assertEquals("10.09.2022", convertTime.fromTimeToString(		1662843600000))
+    }
+
+    @Test
+    fun `success from string to long format with`() {
+        val simpleDateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+        simpleDateFormat.timeZone = TimeZone.getTimeZone("Europe/Moscow")
+        val convertTime = ConvertTime.Base(simpleDateFormat)
+        Assert.assertEquals(1662843600000, convertTime.fromStringToTime("11.09.2022"))
+    }
+
+    @Test
+    fun `success from same string but to different long due to timezone`() {
+        val simpleDateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+        simpleDateFormat.timeZone = TimeZone.getTimeZone("America/Denver")
+        val convertTime = ConvertTime.Base(simpleDateFormat)
+        Assert.assertEquals(1662876000000, convertTime.fromStringToTime("11.09.2022"))
     }
 }
